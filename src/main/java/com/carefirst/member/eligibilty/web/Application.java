dbea56c57942a5399aa06013c3eb086a7f1d8d1d
Chowdripalya, Lokesh <Lokesh.Chowdripalya@carefirst.com>
2022-02-17 23:58:09 -0500

    removed login

--- src/main/java/com/carefirst/member/eligibilty/web/Application.java
+++ src/main/java/com/carefirst/member/eligibilty/web/Application.java
@@ -1,15 +1,29 @@
 package com.carefirst.member.eligibilty.web;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
-@ComponentScan("com.carefirst.member.eligibilty.web.*")
-public class Application {
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@ComponentScan({"com.carefirst.member.eligibilty.web.*","com.carefirst.nexus.gen"})
+public class Application implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		
+		 registry.addResourceHandler("/images/**").addResourceLocations("/WEB-INF/images/")
+         .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
 	}
 
 }
